1d912e95018bfc13a0e9ccc7b8b581f2ac33ec18
lyf <lipu@csii.com.cn>
2019-09-11 08:43:32 +0800

    SpringBoot--@Scheduled定时器用法

--- spring-boot2-schedule/src/main/java/com/md/demo/Application.java
+++ spring-boot2-schedule/src/main/java/com/md/demo/Application.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ spring-boot2-schedule/src/main/java/com/md/demo/ScheduleConfig.java
@@ -0,0 +1,15 @@
+package com.md.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(Executors.newScheduledThreadPool(10));
+    }
+}
